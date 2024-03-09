7dde2364beccd5b5552115ca64bece60e44bfa7f
Aastik Mehta <122990856+Aastik-Mehta-22@users.noreply.github.com>
2024-03-09 22:36:04 +0530

    Update 07_datesinjs.js

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -9,8 +9,8 @@
 // console.log(myCreatedDate.getTime()); // milisecodn me convert kr dega ye 
 // console.log(Math.floor(Date.now()/1000)); // to convert it in second
 
-// let newDate = new Date()
-// console.log(newDate);
-// console.log(newDate.getMonth());
-// console.log(newDate.getMonth()+1);
-// console.log(newDate.getDay());
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
