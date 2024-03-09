d3250bee1001bea589abe697fde5e148b7d62ee7
Aastik Mehta <122990856+Aastik-Mehta-22@users.noreply.github.com>
2024-03-09 14:38:41 +0000

    datatypes or conversions

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3);
+console.log("hitesh");
+
+//datatypes
+//number > 2 to power 53
+//bigint
+//string => ""
+//bool =>t/f
+//null =>stand alone value
+//undefined =>> value assign nhi ki , null ek represenation h empty value ka like let temp = null;
+//symbol => uniqueness
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof age);
---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,14 @@
+let score = "33ABCD";
+console.log(typeof score);
+
+console.log(typeof(score));
+let valueInNumber  = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //nan not  a number
+
+//"33" is easily converted into 33
+//"33abs" is  NaN (Not a Number)
+//true => 1; false => 0;
+
+//conversion in bollelan 
+//1 => true , 0=> false , ""=> false , "aastik" => true
